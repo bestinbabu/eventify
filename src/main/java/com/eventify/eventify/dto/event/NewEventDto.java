d117812317a5e073adace30c3b92a41cf4929793
bestinbabu <bestinbabu6@gmail.com>
2024-07-01 02:19:58 +0530

    after error commit

--- src/main/java/com/eventify/eventify/dto/event/NewEventDto.java
+++ src/main/java/com/eventify/eventify/dto/event/NewEventDto.java
@@ -1,4 +1,14 @@
 package com.eventify.eventify.dto.event;
 
+import java.time.LocalDateTime;
+import java.util.List;
+
 public class NewEventDto {
+
+    private String eventName;
+    private String description;
+    private LocalDateTime eventTime;
+    private Integer numberOfSeats;
+    private Double pricingPerSeat;
+    private List<String> categories;
 }
